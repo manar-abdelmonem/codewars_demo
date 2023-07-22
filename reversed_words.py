5706d204bfdbfc9c3b52d2bd2909d0469020ba4d
manar abdelmonem <111070643+manar-abdelmonem@users.noreply.github.com>
2023-07-22 07:07:50 +0300

    Update reversed_words.py

--- reversed_words.py
+++ reversed_words.py
@@ -1,4 +1,5 @@
-// Complete the solution so that it reverses all of the words within the string passed in.z// Words are separated by exactly one space and there are no leading or trailing spaces.
+// Complete the solution so that it reverses all of the words within the string passed in.
+// Words are separated by exactly one space and there are no leading or trailing spaces.
 // Example(Input --> Output):
 // "The greatest victory is that which requires no battle" --> "battle no requires which that is victory greatest The"
 // my solution
