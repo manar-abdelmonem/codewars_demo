eb1f6b6917f1357a8bc0e232077510287cc3e0bc
manar abdelmonem <111070643+manar-abdelmonem@users.noreply.github.com>
2023-07-22 09:56:58 +0300

    Update keep_hydrated.py

--- keep_hydrated.py
+++ keep_hydrated.py
@@ -4,10 +4,8 @@
  //You get given the time in hours and you need to return the number of litres Nathan will drink, rounded to the smallest value.
  //For example:
        //time = 3 ----> litres = 1
-
-       //time = 6.7---> litres = 3
-
-       //time = 11.8--> litres = 5
-     // solution
+      //time = 6.7---> litres = 3
+     //time = 11.8--> litres = 5
+// solution
        def litres(time):
          return time//2
